bab234715eb1ebf0b0196ef55d42fb1762eef038
ModeTouchE <mnj1254@126.com>
2023-11-21 22:40:53 +0800

    v2.0
    1、添加统一的文件编码，且可自动遍历多个编码，选择未报错的编码进行读取和保存
    2、添加保存选项，可选择是另存为修改后文件或是直接修改原文件

--- changeTimeInAssFile.py
+++ changeTimeInAssFile.py
@@ -4,84 +4,43 @@ import os
 import datetime
 
 
-# 传入一个时间字符串，如果该字符串的分和秒对应位置大于等于60，则向上进位；如果小于0，则向下进位，时分秒如果均小于0，则强制取0，最后返回符合要求的时间字符串
-def solvedCarry(timeStr):
-    tmpList = timeStr.split(":")
-    hourStr = tmpList[0]
-    minStr = tmpList[1]
-    seconedStr = tmpList[2]
-    hourInt = int(hourStr)
-    minInt = int(minStr)
-    seconedFloat = float(seconedStr)
-
-    # 处理秒数
-    if seconedFloat < 0:
-        carryNum = int(abs(int(seconedFloat)) / 60) + 1
-        seconedFloat = seconedFloat + (60.0 * carryNum)
-        minInt = minInt - carryNum
-    elif seconedFloat >= 60:
-        carryNum = int(int(seconedFloat) / 60)
-        seconedFloat = seconedFloat - (60.0 * carryNum)
-        minInt = minInt + carryNum
-    else:
-        pass
-
-    # 处理分钟数
-    if minInt < 0:
-        carryNum = int(abs(minInt) / 60) + 1
-        minInt = minInt + (60 * carryNum)
-        hourInt = hourInt - carryNum
-    elif minInt >= 60:
-        carryNum = int(minInt / 60)
-        minInt = minInt - (60 * carryNum)
-        hourInt = hourInt + carryNum
-    else:
-        pass
-
-    if hourInt < 0:
-        # 当最终计算导致小时为负数时，强制将时间轴归零
-        hourInt = 0
-        minInt = 0
-        seconedFloat = 0.00
-    else:
-        pass
-
-    # 构建时间字符串
-    seconedStr = "{:0>.2f}".format(seconedFloat)
-    seconedStr = "{:0>5}".format(seconedStr)
-    minStr = "{:0>2}".format(minInt)
-    hourStr = "{0}".format(hourStr)
-    tmpList = [hourStr, minStr, seconedStr]
-    reTimeStr = ":".join(tmpList)
-    return reTimeStr
-
-
-# 调整秒速
 def addS(timeStr, addS):
     tmpList = timeStr.split(":")
     minStr = tmpList[1]
     minInt = int(minStr)
     secondStr = tmpList[2]
     addFloat = float(tmpList[2]) + addS
-
+    # 判断是否需要进位
+    if addFloat >59:
+        # 分位进一位
+        minInt = minInt+1
+        addFloat = addFloat-60
+    elif addFloat<0:
+        # 分位退一位
+        minInt = minInt -1
+        addFloat = addFloat+60
+        if minInt<0:
+            minInt=0
+    
     addStr = "{:0>.2f}".format(addFloat)
     addStr = "{:0>5}".format(addStr)
     tmpList[2] = addStr
-    solvedTimeStr = ":".join(tmpList)
-    finalTimeStr = solvedCarry(solvedTimeStr)
-    return finalTimeStr
-
+    minStr = "{:0>2}".format(minInt)
+    tmpList[1] = minStr
+    return ":".join(tmpList)
 
 # 使用前需要修改的变量
-inputFolder = r"E:\pycharm project\new\ass"  # 要输入的字幕文件的保存文件夹
-addSInt = -120  # 要整体移动的秒速，正数为加，负数为减
-supportedSuffixList = [".ass"]  # 支持的字幕后缀名
-assContentStartStr = "Dialogue:"  # 表示要处理的时间轴部分的前缀
-outputParentFolder = "结果"  # 保存生成ass文件的文件夹名
+inputFolder = r"H:\ceshi"    # 要输入的字幕文件的保存文件夹
+addSInt = 1    # 要整体移动的秒速，正数为加，负数为减
+supportedSuffixList = [".ass"]    # 支持的字幕后缀名
+assContentStartStr = "Dialogue:"    # 表示要处理的时间轴部分的前缀
+outputParentFolder = "结果"       # 保存生成ass文件的文件夹名 
+fileEncodeStrList = ["utf-8","utf-16"]           # 读取和保存文件的编码，一般尝试utf-8 和 utf-16
+solvedType = 1                    # 处理文件的方式，0为保存到新的文件夹中，1为直接修改原始文件
 
-nowSaveFolerName = datetime.datetime.now().strftime("%Y%m%d-%X").replace(":", "")
-saveFolderPath = os.path.join(os.getcwd(), outputParentFolder, nowSaveFolerName)
-if not os.path.exists(saveFolderPath):
+nowSaveFolerName = datetime.datetime.now().strftime("%Y%m%d-%X").replace(":","")
+saveFolderPath = os.path.join(os.getcwd(),outputParentFolder, nowSaveFolerName)
+if not os.path.exists(saveFolderPath) and solvedType==0:
     os.makedirs(saveFolderPath)
 
 fileNameList = os.listdir(inputFolder)
@@ -95,13 +54,28 @@ for fileIndex, fileName in enumerate(fileNameList):
         continue
     else:
         fileLines = []
-
-        with open(filePath, "r", encoding="utf-8") as fr:
-            fileLines = fr.readlines()
+        for fileEncodeIndex,fileEncodeStr in enumerate(fileEncodeStrList):
+            try:
+                with open(filePath, "r", encoding=fileEncodeStr) as fr:
+                    fileLines = fr.readlines()
+                break
+            except:
+                if fileEncodeIndex != len(fileEncodeStrList)-1:
+                    continue
+                else:
+                    print("编码错误")
+                    exit(0)
         print("读取到{}行输入".format(len(fileLines)))
         solvedLines = []
-        outputName = os.path.join(saveFolderPath, fileName)
-        with open(outputName, "w+", encoding="utf-8") as fr:
+        
+        if solvedType == 0:
+            outputName = os.path.join(saveFolderPath, fileName)
+            writeType = "w+"
+        elif solvedType == 1:
+            outputName = os.path.join(inputFolder, fileName)
+            writeType = "w"
+        
+        with open(outputName, writeType, encoding=fileEncodeStr) as fr:
             for index, line in enumerate(fileLines):
                 print("\r正在处理{0}/{1}行数据".format(index + 1, len(fileLines)), end="")
                 solvedLine = ""
